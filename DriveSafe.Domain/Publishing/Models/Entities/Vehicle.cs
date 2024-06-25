7285cceb2e01c51601de63e7e96a4468d4e90ec1
Leonel Alfaro <89095594+aliand2@users.noreply.github.com>
2024-06-25 12:23:14 -0500

    Update Vehicle

--- DriveSafe.Domain/Publishing/Models/Entities/Vehicle.cs
+++ DriveSafe.Domain/Publishing/Models/Entities/Vehicle.cs
@@ -7,5 +7,13 @@ public class Vehicle : BaseModel
     public int MaximumSpeed { get; set; }
     public int Consumption { get; set; }
     public string Dimensions { get; set; }
-
+    public int Weight { get; set; }
+    public string CarClass { get; set; }
+    public string Transmission { get; set; }
+    public string TimeType { get; set; }
+    public int RentalCost { get; set; }
+    public string PickUpPlace { get; set; }
+    public string UrlImage { get; set; }
+    public string RentStatus { get; set; }
+    public int OwnerId { get; set; }
 }
